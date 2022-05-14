9d721e75f0ff3987ab71715f8e176ce5beb4b7ff
ryanrasoarahona3 <ryanrasoarahona3@gmail.com>
2022-05-14 07:33:48 +0300

    add example servlet

---
+++ src/main/java/com/example/cryptosim/ExampleServlet.java
@@ -0,0 +1,16 @@
+package com.example.cryptosim;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name= "example", value="/example")
+public class ExampleServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("/page.jsp").forward(request, response);
+    }
+}
---
+++ src/main/webapp/page.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: astrozeneka
+  Date: 14/05/2022
+  Time: 07:29
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>The Page</title>
+</head>
+<body>
+    This is the page
+</body>
+</html>
