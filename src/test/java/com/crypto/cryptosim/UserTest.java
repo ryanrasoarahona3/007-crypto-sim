2d207d71488801ff9f1694b2f6609ef19489c186
ryan <ryanrasoarahona@gmail.com>
2022-05-15 12:06:53 +0300

    Test userCredentials

--- src/test/java/com/crypto/cryptosim/UserTest.java
+++ src/test/java/com/crypto/cryptosim/UserTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class UserTest extends AbstractTest {
@@ -24,5 +25,26 @@ public class UserTest extends AbstractTest {
         ur.add(u);
 
         assertNotEquals((Integer) null, u.getId());
+    }
+
+    @Test
+    void verifyCredentials() throws SQLException {
+        User u1 = new User();
+        u1.setEmail("john@gmail.com");
+        u1.setPassword("password");
+        ur.add(u1);
+        User u = new User();
+
+        u.setEmail("john@gmail.com");
+        u.setPassword("pass");
+        assertEquals(false, ur.verifyCredentials(u));
+
+        u.setEmail("john@gmail.co");
+        u.setPassword("password");
+        assertEquals(false, ur.verifyCredentials(u));
+
+        u.setEmail("john@gmail.com");
+        u.setPassword("password");
+        assertEquals(true, ur.verifyCredentials(u));
     }
 }
